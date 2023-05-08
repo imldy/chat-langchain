990267074c6c037fb6b78118391418c747b7692c
imldy <im.ldy@outlook.com>
2023-05-08 13:44:32 +0800

    增加注释

--- get_vectorstores.py
+++ get_vectorstores.py
@@ -11,11 +11,15 @@ def 获得黄海集市资料的Vectorstores():
 
     # 切分文档
     text_splitter = RecursiveCharacterTextSplitter(
+        # 每个文档快大小
         chunk_size=1000,
+        # 每两个文档重叠的快大小
         chunk_overlap=200,
     )
     doc = text_splitter.split_documents(documents=raw_doc)
+    # 获得一个嵌入模型对象，用以将文档低维化，获得token的低维向量，用以对比相关性
     embedding = OpenAIEmbeddings()
+    # FAISS是Facebook AI 相似性搜索 (Faiss)，是一个用于高效相似性搜索和密集向量聚类的库
     fs: FAISS = FAISS.from_documents(documents=doc, embedding=embedding)
     fs.save_local("hhjsdoc")
 
