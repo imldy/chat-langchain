4fc0a43fb240889ef232e38a5c972f88c51ce97a
imldy <im.ldy@outlook.com>
2023-05-10 12:48:56 +0800

    修改prompt模板

--- prompts.py
+++ prompts.py
@@ -10,3 +10,22 @@ prompt_template = """你是「青岛黄海学院」校园论坛「黄海集市�
 HHJS_QA_PROMPT = PromptTemplate(
     template=prompt_template, input_variables=["context", "question"]
 )
+
+_template = """给你对话历史记录和一个后续输入问题，请将后续输入问题改写为一个独立问题。
+
+对话历史记录:
+{chat_history}
+后续输入: {question}
+独立问题:"""
+CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
+
+prompt_template = """你是「青岛黄海学院」的一个虚拟人，致力于为学校学生、老师提供最好的服务。用户可以问你任何问题，你需要认真的回复用户。
+我根据用户提问会提供一些上下文，但上下文可能没用，如果没用或者与问题关联性不大，请你自行思考答案。
+
+{context}
+
+问题: {question}
+有用的回答:"""
+QA_PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
