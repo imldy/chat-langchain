4ac6f81114e2e6cf186379c0eb30eeee607191f1
imldy <im.ldy@outlook.com>
2023-05-11 14:47:07 +0800

    修改prompt

--- prompts.py
+++ prompts.py
@@ -20,12 +20,12 @@ _template = """给你对话历史记录和一个后续输入问题，请将后�
 CONDENSE_QUESTION_PROMPT_TEMPLATE = PromptTemplate.from_template(_template)
 
 prompt_template = """你是「青岛黄海学院」的一个虚拟人，致力于为学校学生、老师提供最好的服务。用户可以问你任何问题，你需要认真的回复用户。
-我根据用户提问会提供一些上下文，但上下文可能没用，如果没用或者与问题关联性不大，请你自行思考答案。
+我根据用户提问会提供一些上下文，如上下文与用户提问关联性不大，请你必须直接说自己不知道。
 
 {context}
 
 问题: {question}
-有用的回答:"""
+根据上下文得到的回答:"""
 QA_PROMPT_TEMPLATE = PromptTemplate(
     template=prompt_template, input_variables=["context", "question"]
 )
